76dc7d073290d1b952b3663561a5ab213f5e4298
Dmitriy Kalinin <cppforlife@gmail.com>
2020-03-14 11:34:20 -0400

    add e2e for secret exports

---
+++ test/e2e/secret_exports_test.go
@@ -0,0 +1,175 @@
+package e2e
+
+import (
+	"strings"
+	"testing"
+	"reflect"
+	"time"
+
+	"github.com/ghodss/yaml"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestExportSuccessful(t *testing.T) {
+	env := BuildEnv(t)
+	logger := Logger{}
+	kapp := Kapp{t, env.Namespace, logger}
+	kubectl := Kubectl{t, env.Namespace, logger}
+
+	yaml1 := `
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: sg-test1
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: sg-test2
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: sg-test3
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: secret
+  namespace: sg-test1
+type: Opaque
+stringData:
+  key1: val1
+  key2: val2
+  key3: val3
+---
+apiVersion: secretgen.k14s.io/v1alpha1
+kind: SecretExport
+metadata:
+  name: secret
+  namespace: sg-test1
+spec:
+  toNamespaces:
+  - sg-test2
+  - sg-test3
+---
+apiVersion: secretgen.k14s.io/v1alpha1
+kind: SecretRequest
+metadata:
+  name: secret
+  namespace: sg-test2
+spec:
+  fromNamespace: sg-test1
+---
+apiVersion: secretgen.k14s.io/v1alpha1
+kind: SecretRequest
+metadata:
+  name: secret
+  namespace: sg-test3
+spec:
+  fromNamespace: sg-test1
+`
+
+	yaml2 := `
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: secret
+  namespace: sg-test1
+type: Opaque
+stringData:
+  key1: val1.1 # update
+               # key2 deleted
+  key3: val3   # keep
+  key4: val4   # new
+`
+
+	name := "test-export-successful"
+	cleanUp := func() {
+		kapp.RunWithOpts([]string{"delete", "-a", name}, RunOpts{AllowError: true})
+	}
+
+	cleanUp()
+	defer cleanUp()
+
+	logger.Section("Deploy", func() {
+		kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name},
+			RunOpts{StdinReader: strings.NewReader(yaml1)})
+	})
+
+	logger.Section("Check imported secrets were created", func() {
+		for _, ns := range []string{"sg-test2", "sg-test3"} {
+			out := waitForSecretInNs(t, kubectl, ns, "secret")
+
+			var secret corev1.Secret
+
+			err := yaml.Unmarshal([]byte(out), &secret)
+			if err != nil {
+				t.Fatalf("Failed to unmarshal: %s", err)
+			}
+
+			if secret.Type != "Opaque" {
+				t.Fatalf("Wrong type")
+			}
+			expectedData := map[string][]byte{
+				"key1": []byte("val1"),
+				"key2": []byte("val2"),
+				"key3": []byte("val3"),
+			}
+			if !reflect.DeepEqual(secret.Data, expectedData) {
+				t.Fatalf("Expected secret data to match, but was: %#v vs %s", secret.Data, out)
+			}
+		}
+	})
+
+	logger.Section("Update secret", func() {
+		kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name, "-p"},
+			RunOpts{StdinReader: strings.NewReader(yaml2)})
+	})
+
+	logger.Section("Check imported secrets were updated", func() {
+		// TODO proper waiting
+		time.Sleep(5*time.Second)
+
+		for _, ns := range []string{"sg-test2", "sg-test3"} {
+			out := waitForSecretInNs(t, kubectl, ns, "secret")
+
+			var secret corev1.Secret
+
+			err := yaml.Unmarshal([]byte(out), &secret)
+			if err != nil {
+				t.Fatalf("Failed to unmarshal: %s", err)
+			}
+
+			if secret.Type != "Opaque" {
+				t.Fatalf("Wrong type")
+			}
+			expectedData := map[string][]byte{
+				"key1": []byte("val1.1"),
+				"key3": []byte("val3"),
+				"key4": []byte("val4"),
+			}
+			if !reflect.DeepEqual(secret.Data, expectedData) {
+				t.Fatalf("Expected secret data to match, but was: %#v vs %s", secret.Data, out)
+			}
+		}
+	})
+
+	logger.Section("Delete export to see exported secrets deleted", func() {
+		kubectl.RunWithOpts([]string{"delete", "secretexport", "secret", "-n", "sg-test1"},
+			RunOpts{NoNamespace: true})
+
+		// TODO proper waiting
+		time.Sleep(5*time.Second)
+
+		for _, ns := range []string{"sg-test2", "sg-test3"} {
+			_, err := kubectl.RunWithOpts([]string{"get", "secret", "secret", "-n", ns},
+				RunOpts{AllowError: true})
+
+			if !strings.Contains(err.Error(), "(NotFound)") {
+				t.Fatalf("Expected NotFound error but was: %s", err)
+			}
+		}
+	})
+}
--- test/e2e/wait.go
+++ test/e2e/wait.go
@@ -6,12 +6,23 @@ import (
 )
 
 func waitForSecret(t *testing.T, kubectl Kubectl, name string) string {
+	return waitForSecretInNs(t, kubectl, "", name)
+}
+
+func waitForSecretInNs(t *testing.T, kubectl Kubectl, nsName, name string) string {
 	var lastErr error
+
+	args := []string{"get", "secret", name, "-o", "yaml"}
+	noNs := false
+
+	if len(nsName) > 0 {
+		args = append(args, []string{"-n", nsName}...)
+		noNs = true
+	}
 
 	for i := 0; i < 30; i++ {
 		var out string
-		out, lastErr = kubectl.RunWithOpts([]string{"get", "secret", name, "-o", "yaml"},
-			RunOpts{AllowError: true})
+		out, lastErr = kubectl.RunWithOpts(args, RunOpts{AllowError: true, NoNamespace: noNs})
 		if lastErr == nil {
 			return out
 		}
