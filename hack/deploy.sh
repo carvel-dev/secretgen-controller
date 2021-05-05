a0a1f4f890e77f71a2b17a46672db07f153c7b2b
Dmitriy Kalinin <dkalinin@vmware.com>
2021-05-05 18:02:49 -0400

    deploy script should work against minikube

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-./hack/build.sh && ytt -f config/ --data-value-yaml=push_images=true -v image_repo=docker.io/dkalinin/test | kbld -f- | kapp deploy -a sg -f- -c -y
+./hack/build.sh && ytt -f config/ | kbld -f- | kapp deploy -a sg -f- -c -y
