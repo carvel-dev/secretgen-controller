0a6201d58cc1c29b78fd5e9a0f16f09386da9e28
Joe Kimmel <jkimmel@vmware.com>
2021-08-11 11:43:00 -0700

    add copyright headers to all files

--- pkg/apis/secretgen/v1alpha1/doc.go
+++ pkg/apis/secretgen/v1alpha1/doc.go
@@ -1,3 +1,6 @@
+// Copyright 2021 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 // +k8s:deepcopy-gen=package
 // +k8s:defaulter-gen=TypeMeta
 // +groupName=secretgen
--- pkg/expansion/doc.go
+++ pkg/expansion/doc.go
@@ -1,3 +1,6 @@
+// Copyright 2021 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 // Package taken from https://github.com/kubernetes/kubernetes/tree/a98c559ca4b6946bdf15fcbad7d5c450283c2f9f/third_party/forked/golang/expansion
 
 package expansion
