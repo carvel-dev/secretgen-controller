fbef0ea049781ada9e68956fa6a60a24982cb194
Dmitriy Kalinin <dkalinin@vmware.com>
2021-08-18 10:43:07 -0400

    crystalize rules for matched secret order

--- pkg/sharing/secret_exports.go
+++ pkg/sharing/secret_exports.go
@@ -4,12 +4,21 @@
 package sharing
 
 import (
-	"sync"
+	"fmt"
 	"sort"
+	"strconv"
+	"sync"
 
 	"github.com/go-logr/logr"
 	sgv1alpha1 "github.com/vmware-tanzu/carvel-secretgen-controller/pkg/apis/secretgen/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
+)
+
+const (
+	// WeightAnnKey allows to control which secrets are preferred to others
+	// during fulfillment of secret requests. It's especially handy for
+	// controlling how multiple image pull secrets are merged together.
+	WeightAnnKey = "secretgen.carvel.dev/weight"
 )
 
 // SecretExports is an in-memory cache of exported secrets.
@@ -23,10 +32,13 @@ type SecretExports struct {
 	exportedSecrets     map[string]exportedSecret
 }
 
+// NewSecretExports constructs new SecretExports cache.
 func NewSecretExports(log logr.Logger) *SecretExports {
 	return &SecretExports{log: log, exportedSecrets: map[string]exportedSecret{}}
 }
 
+// Export adds the in-memory representation (cached)
+// of both the SecretExport and underlying Secret.
 func (se *SecretExports) Export(export *sgv1alpha1.SecretExport, secret *corev1.Secret) {
 	if secret == nil {
 		panic("Internal inconsistency: expected non-nil secret")
@@ -50,6 +62,7 @@ func (se *SecretExports) Unexport(export *sgv1alpha1.SecretExport) {
 	delete(se.exportedSecrets, exportedSec.Key())
 }
 
+// SecretMatcher allows to specify criteria for matching exported secrets.
 type SecretMatcher struct {
 	Namespace  string
 	Subject    string
@@ -57,26 +70,36 @@ type SecretMatcher struct {
 }
 
 // MatchedSecretsForImport filters secrets export cache by the given criteria.
+// Returned order (last in the array is most specific):
+//   - secret with highest weight? (default weight=0), or
+//     - secret within the same namespace
+//       - secret with specific namespace
+//       - secret with wildcard namespace match
+//     - secret within other namespaces
+//       - secret with specific namespace
+//       - secret with wildcard namespace match
+//     (in all cases fallback to secret namespace/name sort)
 func (se *SecretExports) MatchedSecretsForImport(matcher SecretMatcher) []*corev1.Secret {
 	se.exportedSecretsLock.RLock()
 	defer se.exportedSecretsLock.RUnlock()
 
-	var result []*corev1.Secret
+	var matched []exportedSecret
 
 	for _, exportedSec := range se.exportedSecrets {
 		if exportedSec.Matches(matcher) {
-			result = append(result, exportedSec.Secret())
+			matched = append(matched, exportedSec)
 		}
 	}
 
-	// Return determinsticly ordered result
-	sort.Slice(result, func(i, j int) bool {
-		// First by namespace, then by name
-		if result[i].Namespace != result[j].Namespace {
-			return result[i].Namespace < result[j].Namespace
-		}
-		return result[i].Name < result[j].Name
+	sort.Slice(matched, func(i, j int) bool {
+		// j and i are flipped to do a reverse sort
+		return matched[j].SortKey(matcher.Namespace).Less(matched[i].SortKey(matcher.Namespace))
 	})
+
+	var result []*corev1.Secret
+	for _, exportedSec := range matched {
+		result = append(result, exportedSec.Secret())
+	}
 
 	return result
 }
@@ -135,4 +158,64 @@ func (es exportedSecret) matchesNamespace(nsToMatch string) bool {
 		}
 	}
 	return false
+}
+
+func (es exportedSecret) SortKey(dstNs string) exportedSecretSortKey {
+	var weight float64 // default weight is 0.0
+	if val, found := es.export.Annotations[WeightAnnKey]; found {
+		if typedVal, err := strconv.ParseFloat(val, 64); err == nil { // Ignore invalid weights
+			weight = typedVal
+		}
+	}
+
+	var matchesNsExactly bool
+	for _, ns := range es.export.StaticToNamespaces() {
+		if ns == dstNs {
+			matchesNsExactly = true
+			break
+		}
+	}
+
+	return exportedSecretSortKey{
+		Weight:           weight,
+		WithinDstNs:      es.secret.Namespace == dstNs,
+		MatchesNsExactly: matchesNsExactly,
+		SecretNsName:     fmt.Sprintf("%s/%s", es.secret.Namespace, es.secret.Name),
+	}
+}
+
+type exportedSecretSortKey struct {
+	Weight           float64
+	WithinDstNs      bool
+	MatchesNsExactly bool // or by wildcard
+	SecretNsName     string
+}
+
+func (k exportedSecretSortKey) Less(otherKey exportedSecretSortKey) bool {
+	// Check weights
+	if k.Weight > otherKey.Weight {
+		return true
+	}
+	if k.Weight < otherKey.Weight {
+		return false
+	}
+
+	// Check same dst namespace
+	if k.WithinDstNs && !otherKey.WithinDstNs {
+		return true
+	}
+	if !k.WithinDstNs && otherKey.WithinDstNs {
+		return false
+	}
+
+	// Check ns name exact match
+	if k.MatchesNsExactly && !otherKey.MatchesNsExactly {
+		return true
+	}
+	if !k.MatchesNsExactly && otherKey.MatchesNsExactly {
+		return false
+	}
+
+	// Lastly just by ns/name
+	return k.SecretNsName > otherKey.SecretNsName
 }
