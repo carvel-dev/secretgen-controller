52be7537c79c0d2967e6afa823827da5bedcddeb
Eli Wrenn <ewrenn@pivotal.io>
2021-08-18 12:08:48 -0700

    regenerate crd clients
    regenerate with the right group name this time

--- hack/gen.sh
+++ hack/gen.sh
@@ -11,3 +11,5 @@ rm -rf pkg/client
 ./vendor/k8s.io/code-generator/generate-groups.sh \
 	all github.com/vmware-tanzu/carvel-secretgen-controller/pkg/client github.com/vmware-tanzu/carvel-secretgen-controller/pkg/apis secretgen:v1alpha1 \
 	--go-header-file ./hack/gen-boilerplate.txt
+
+chmod -x $gen_groups_path
