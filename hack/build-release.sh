56e03b09373550f3c64e29d2c9b9d34da0b91cba
Neil Hickey <nhickey@vmware.com>
2022-06-06 12:50:26 -0600

    Add imgpkg pushing for bundle
    Signed-off-by: Neil Hickey <nhickey@vmware.com>

--- hack/build-release.sh
+++ hack/build-release.sh
@@ -6,7 +6,13 @@ source ./hack/version-util.sh
 
 mkdir -p tmp/
 
-ytt -f config/ -f config-build -f config-deploy -f config-release -v secretgen_controller_version="$(get_sgctrl_ver)" | kbld -f- > ./tmp/release.yml
+ytt -f config/ -f config-build -f config-deploy -f config-release -v secretgen_controller_version="$(get_sgctrl_ver)" | kbld --imgpkg-lock-output config/.imgpkg/images.yml -f- > ./tmp/release.yml
+
+imgpkg push -b ghcr.io/vmware-tanzu/carvel-secretgen-controller-package-bundle:"$(get_sgctrl_ver)" -f config --lock-output ./bundle-image.yml
+
+ytt -f packaging/package.yml -f packaging/values.yml -v version="$(get_sgctrl_ver)" -v image="$(yq eval '.bundle.image' bundle-image.yml)" > ./tmp/package.yml
+
+cp packaging/metadata.yml ./tmp/metadata.yml
 
 shasum -a 256 ./tmp/release*.yml
 
