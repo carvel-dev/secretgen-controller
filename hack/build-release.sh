6ef716957e3005a4156c6e484045f8742897be39
Neil Hickey <nhickey@vmware.com>
2022-06-07 17:25:34 -0600

    Use openapi generated schema for package
    Signed-off-by: Neil Hickey <nhickey@vmware.com>

--- hack/build-release.sh
+++ hack/build-release.sh
@@ -15,10 +15,14 @@ ytt -f config/package-bundle/contents -f config/release -v dev.version="$version
 
 imgpkg push -b ghcr.io/vmware-tanzu/carvel-secretgen-controller-package-bundle:"$version" -f config/package-bundle --lock-output ./bundle-image.yml
 
-ytt -f config/package/package.yml -f config/package/values.yml -v version="$version" -v image="$(yq eval '.bundle.image' bundle-image.yml)" > ./tmp/package.yml
+# generate openapi schema for package
+ytt -f config/package-bundle/contents --data-values-schema-inspect -o openapi-v3 > ./schema-openapi.yml
+
+ytt -f config/package/package.yml -f config/package/values.yml --data-value-file openapi=./schema-openapi.yml -v version="$version" -v image="$(yq eval '.bundle.image' bundle-image.yml)" > ./tmp/package.yml
 
 cp config/package/metadata.yml ./tmp/metadata.yml
 rm ./bundle-image.yml
+rm ./schema-openapi.yml
 
 shasum -a 256 ./tmp/release.yml
 shasum -a 256 ./tmp/package.yml
