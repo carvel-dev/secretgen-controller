c7839863f9edeb8b418c0d40875835edd03101e5
Yash Sethiya <ysethiya@Yashs-MacBook-Pro.local>
2022-06-22 16:06:18 +0530

    release action

--- test/e2e/secret_exports_test.go
+++ test/e2e/secret_exports_test.go
@@ -10,8 +10,8 @@ import (
 	"time"
 
 	"github.com/ghodss/yaml"
-	corev1 "k8s.io/api/core/v1"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 )
 
 func TestExportSuccessful(t *testing.T) {
