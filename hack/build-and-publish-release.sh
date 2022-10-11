1864c59ed79e76fa4c2d127078470420320b92e4
sethiyash <yashsethiya97@gmail.com>
2022-10-12 01:18:10 +0530

    passing images.yml to kcrl

--- hack/build-and-publish-release.sh
+++ hack/build-and-publish-release.sh
@@ -5,13 +5,13 @@ set -e -x -u
 source ./hack/version-util.sh
 
 mkdir -p tmp/
-mkdir -p config/package-bundle/.imgpkg/
+mkdir -p .imgpkg/
 
 export version="$(get_sgctrl_ver)"
 export version_without_v_prefix="$(get_sgctrl_ver_without_v)"
 
 yq eval '.metadata.annotations."secretgen-controller.carvel.dev/version" = env(version)' -i "config/package-bundle/config/deployment.yml"
 
-ytt -f config/package-bundle/config -f config/release -v dev.version="$version_without_v_prefix" | kbld --imgpkg-lock-output config/package-bundle/.imgpkg/images.yml -f- > ./tmp/release.yml
+ytt -f config/package-bundle/config -f config/release -v dev.version="$version_without_v_prefix" | kbld --imgpkg-lock-output .imgpkg/images.yml -f- > ./tmp/release.yml
 
 echo SUCCESS
